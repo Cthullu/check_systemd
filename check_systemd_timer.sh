10573a817dbba58f4c0cc9c0351308e37736e9ea
Daniel Kuß <d.kuss86@googlemail.com>
2023-10-14 12:06:51 +0200

    Save first darft

---
+++ check_systemd_timer.sh
@@ -0,0 +1,96 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+#
+# This is a lightweight bash script to be used with Nagios/Icinga using only
+# bash functionality to keep dependencies at a minimum.
+#
+
+#
+# Author: Daniel Kuß
+# License: MIT
+# Source: https://github.com/Cthullu/icinga_timer_check
+#
+
+VERSION="0.1.0"
+
+#
+# Function to print tool usage
+#
+
+Usage()
+{
+    echo
+    echo "Usage: ${0} [-h|v] [-t <int>] [-u <string>]" 1>&2
+    echo
+}
+
+#
+# Function to print help message
+#
+
+Help()
+{
+    # Display the help message
+    echo "Systemd Timer check script."
+    echo 
+    echo "This script can be used to get the current state of a systemd timer."
+    echo "If only the timer name is provided, the script will check if the"
+    echo "timer is active."
+    echo "If in addition a time-window is provided, the script will check if"
+    echo "the timer had run within given window."
+
+    Usage
+
+    echo "Options:"
+    echo "-h    Print this help message."
+    echo "-t    Timeframe in minutes since the unit should have last run."
+    echo "-u    Name of the systemd timer unit to be checked."
+    echo "-v    Print the script version."
+    echo
+}
+
+#
+# Main program body
+#
+
+
+#
+# Get the command line opts
+#
+
+while getopts ":ht:u:v" option; do
+    case ${option} in
+        h)  # Print the help message
+            Help
+            exit 0
+            ;;
+        t)  
+            TIMEFRAME=${OPTARG}
+            re_isanum='^[0-9]+$'
+            if ! [[ ${TIMEFRAME} =~ ${re_isanum} ]]; then
+                echo "Error: Timeframe must be a positive integer."
+                Usage
+            fi
+            ;;
+        u)  
+            SYSTEMD_TIMER="${OPTARG}"
+            ;;
+        v)  # Print the version
+            echo "Version: ${VERSION}"
+            exit 0
+            ;;
+        :)
+            echo "Error: -${OPTARG} requires an argument."
+            Usage
+            ;;
+        \?) # Catch invalid options
+            echo "Error: Invalid option"
+            Usage
+            exit 1
+            ;;
+    esac
+done
+
+exit 0
